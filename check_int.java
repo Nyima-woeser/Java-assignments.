8718b30f6c21e4f762451bf5b4c3dc98481199ee
Nyima-woeser <106057516+Nyima-woeser@users.noreply.github.com>
2023-03-30 14:22:46 +0530

    Update and rename Day_3.java to check_int.java

---
+++ check_int.java
@@ -0,0 +1,12 @@
+Class check_int{
+   Public static void main(Strong[] let){
+      int num = 3;
+      if (num >= 0){
+         System.out.println("the given number is positive");
+      }
+      else{
+         System.out.println("the given number is negative");
+      }
+   return
+   }
+}
